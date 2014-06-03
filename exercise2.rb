bd83df5d8627f1960870a45b58abfb396a573185
Andrew Bolt <andrewebolt@gmail.com>
2014-06-03 14:19:10 -0400

    Exercise 2 compete

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+
+#Exercise 2.1. Calculating a good tip on a $55 meal by calculating 20%
+
+meal = 55
+tip = meal * 0.2
+
+puts "A good tip for a $55 meal woul
+d be $#{tip}"
+
+#Exercise 2.2. Adding an integer to a string
+puts "Trying to add the integer\s" + 6.to_s
+
+#Exercise 2.3. Multiplying in a string
+first_number = 45628
+second_number = 7839
+answer = first_number * second_number
+puts "#{first_number} multiplied by #{second_number} equals #{answer}."
+
+#Exercise 2.4. Testing booleans
+(true && false) || (false && true) || !(false && false) 
+=> true
+
+
